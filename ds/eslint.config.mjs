aa37cdf56076afb1660a21ef235e74b9f9cd91e1
cian258 <87565423+dlwjdwls08@users.noreply.github.com>
2025-03-02 14:28:28 +0900

    Update eslint.config.mjs

--- ds/eslint.config.mjs
+++ ds/eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          "vars": "all",
+          "args": "after-used",
+          "ignoreRestSiblings": true,
+          "varsIgnorePattern": "^_", // 밑줄(_)로 시작하는 변수는 무시
+        },
+      ],
+      "unused-imports/no-unused-imports": "off", // 불필요한 import 에러 끄기
+    },
+  },
 ];
 
 export default eslintConfig;
