8d4090b8469c3eb10dfe2c0ecdfd9ae1e4a2f6a3
cian258 <87565423+dlwjdwls08@users.noreply.github.com>
2025-03-19 14:12:05 +0900

    .

--- ds/src/app/login/page.tsx
+++ ds/src/app/login/page.tsx
@@ -1,5 +1,18 @@
 'use client'
 
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/router";
+
 export default function Page() {
-    
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    console.log(session);
+
+    return (
+        <div>
+            <h1>로그인 페이지</h1>
+            <button onClick={() => signIn("google", { callbackUrl: "/" })} />
+        </div>
+    )
 }
