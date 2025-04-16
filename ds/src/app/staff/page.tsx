61844fd29bbbc10eb269254fb4b625f91d13b52b
cian258 <87565423+dlwjdwls08@users.noreply.github.com>
2025-04-16 20:06:23 +0900

    Update page.tsx

--- ds/src/app/staff/page.tsx
+++ ds/src/app/staff/page.tsx
@@ -1,39 +1,18 @@
 'use client'
 
-import { Settings } from "@mui/icons-material"
-import { Box, Button, Container, Paper, Typography } from "@mui/material"
+import { Book, People, School, Settings } from "@mui/icons-material"
+import { Backdrop, Box, Button, Container, Paper, SpeedDial, SpeedDialAction, SpeedDialIcon, Typography } from "@mui/material"
 import axios from "axios"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 import * as XLSX from "xlsx"
 import PageButton from "@/components/pagebutton/pagebutton"
 
 
 export default function StaffPage() {
-  const [getNightClassFile, setNightClassFile] = useState<File | null>(null)
+  const [speedDialOpen, setSpeedDialOpen] = useState(false)
 
-  function handleFileInput(setter: Dispatch<SetStateAction<File | null>>) {
-    return (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.files) {
-        setter(event.target.files[0])
-      }
-    }
-  }
-
-  async function handleUpload() {
-    // axios.post("/api/staff", getNightClassFile)
-    const fileBuffer = Buffer.from(await getNightClassFile!.arrayBuffer())
-    
-    const workbook = XLSX.read(fileBuffer, { type: "buffer" })
-    const sheetName = workbook.SheetNames[0]
-    const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName])
-
-    console.log(data)
-  }
-
-  useEffect(() => {
-    
-  }, [getNightClassFile])
+  const router = useRouter()
 
   return (
     <Box
@@ -44,18 +23,35 @@ export default function StaffPage() {
       width="100%"
       height="100%">
       
-      <PageButton
-        title="야간 수업"
-        url="/staff/setting/night_class" />
-      <PageButton
-        title="학생 목록" 
-        url="/staff/setting/students" />
-      <PageButton
-        title="교사 목록"
-        url="/staff/setting/teacher" />
-      <PageButton
-        title="자습 이석"
-        url="/staff/setting/leave" />
+      <Backdrop
+        open={speedDialOpen} />
+      <SpeedDial
+        ariaLabel=""
+        sx={{
+          position: "absolute",
+          bottom: 20,
+          right: 20
+        }}
+        icon={<Settings />}
+        onClose={() => setSpeedDialOpen(false)}
+        onOpen={() => setSpeedDialOpen(true)}
+        open={speedDialOpen}>
+        <SpeedDialAction
+          icon={<People />}
+          tooltipOpen
+          tooltipTitle="학생"
+          onClick={() => router.push("/staff/setting/students")} />
+        <SpeedDialAction
+          icon={<Book />}
+          tooltipOpen
+          tooltipTitle="수업"
+          onClick={() => router.push("/staff/setting/night_class")} />
+        <SpeedDialAction
+          icon={<School />}
+          tooltipOpen
+          tooltipTitle="교사"
+          onClick={() => router.push("/staff/setting/teacher")} />
+      </SpeedDial>
     </Box>
   )
 }
