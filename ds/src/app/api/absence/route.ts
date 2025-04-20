3998780a8e6ba503bc98b6326fe6d3ca41abc679
cian258 <87565423+dlwjdwls08@users.noreply.github.com>
2025-04-21 00:36:17 +0900

    .

--- ds/src/app/api/absence/route.ts
+++ ds/src/app/api/absence/route.ts
@@ -1,15 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import exp from "constants";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"
+
+dayjs.extend(utc)
 
 const prisma = new PrismaClient();
 
 // get Absence List of the date
-export async function GET(req: NextRequest, {params} : {params : {date : Date}}) {
-	const {date} = await params
+export async function GET(req: NextRequest) {
+	const today = dayjs.utc(new Date())
+	const start = new Date(today.year(), today.month(), today.date());
+	const tomorrow = today.add(1, 'day')
+	const end = new Date(tomorrow.year(), tomorrow.month(), tomorrow.date());
 	const users = await prisma.absenceLog.findMany({
 		where: {
-			date: date
+			date: {
+				gte: start,
+				lte: end
+			}
 		}, 
 		orderBy: {
 			studentID: 'asc'
