16d94250221a151465a8a67be293767215564f18
Jaebin <95126681+jb1420@users.noreply.github.com>
2025-04-21 20:48:26 +0900

    .

--- ds/src/app/api/room/[id]/route.ts
+++ ds/src/app/api/room/[id]/route.ts
@@ -1,12 +1,48 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import dayjs from "dayjs";
 
 const prisma = new PrismaClient()
 
 // get Students in the room
+// export async function GET(req:NextRequest, {params}: { params: Promise<{ id: string }> }) {
+//     try {
+        
+//         const { id } = await params
+//         const room = await prisma.room.findUnique({ 
+//             where: {
+//                 id: Number(id)
+//             },
+//         })
+//         if (!room) {
+//             return NextResponse.json({ error: "Room not found" }, { status: 404 })
+//         }
+
+//         const students = await prisma.student.findMany({
+//             where: {
+//                 room: room.name
+//             },
+//             orderBy: {
+//                 studentID: 'asc'
+//             }
+//         })
+
+//         return NextResponse.json(
+//             {
+//                 students: students,
+//                 room: room
+//             }
+//         )
+//     }
+//     catch {
+//         return NextResponse.json({ error: "DB Error" }, { status: 500 })
+//     }
+// }
+
+
+// get Students + Data in the room
 export async function GET(req:NextRequest, {params}: { params: Promise<{ id: string }> }) {
     try {
-        
         const { id } = await params
         const room = await prisma.room.findUnique({ 
             where: {
@@ -17,6 +53,9 @@ export async function GET(req:NextRequest, {params}: { params: Promise<{ id: str
             return NextResponse.json({ error: "Room not found" }, { status: 404 })
         }
 
+        const now = new Date()
+        const today = dayjs(now)
+
         const students = await prisma.student.findMany({
             where: {
                 room: room.name
@@ -26,12 +65,15 @@ export async function GET(req:NextRequest, {params}: { params: Promise<{ id: str
             }
         })
 
-        return NextResponse.json(
-            {
-                students: students,
-                room: room
+        const memos = await prisma.memo.findMany({
+            where:{ 
+                time: {
+                    scdsv
+                }
             }
-        )
+        })
+
+
     }
     catch {
         return NextResponse.json({ error: "DB Error" }, { status: 500 })
