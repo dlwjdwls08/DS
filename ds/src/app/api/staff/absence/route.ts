29a410e7885db0583f205c0656a056c54ac38cc0
cian258 <87565423+dlwjdwls08@users.noreply.github.com>
2025-04-22 19:53:44 +0900

    .

--- ds/src/app/api/staff/absence/route.ts
+++ ds/src/app/api/staff/absence/route.ts
@@ -19,17 +19,18 @@ export async function GET(req: NextRequest) {
         }
         const now = dayjs(new Date())
         const absences = await prisma.$queryRaw`
-        SELECT S.grade as grade, S."classNo" as classNo, S."studentID" as studentID, S.name as name, AL.date as date, AL.state as state, T.name as teacher
+        SELECT S.grade as grade, S."classNo" as classNo, S."studentID" as studentID, S.name as name, AL.date as date, AL.state as state, T.name as teacher, NC.day
+        -- SELECT *
         FROM "Student" as S
         LEFT JOIN "AbsenceLog" as AL
         ON S."studentID" = AL."studentID" AND AL."date" >= ${new Date(start)} AND AL."date" <= ${new Date(end)}
         INNER JOIN "Teacher" as T
         ON S."classNo" = T."classNo" AND (S.grade = T.grade OR S."classNo" LIKE 'RAA%')
         LEFT JOIN "Leave" as L
-        ON S."studentID" = L."studentID" AND AL.date >= L.date AND AL.date <= L.date + INTERVAL '1 DAY'
+        ON S."studentID" = L."studentID" AND ${new Date(start)} >= L.date AND ${new Date(start)} <= L.date + INTERVAL '1 DAY'
         LEFT JOIN "NightClass" as NC
-        ON S."studentID" = NC."studentID" AND CASE EXTRACT(DOW FROM AL.date) WHEN 0 THEN '일' WHEN 1 THEN '월' WHEN 2 THEN '화' WHEN 3 THEN '수' WHEN 4 THEN '목' WHEN 5 THEN '금' WHEN 6 THEN '토' END = NC.day AND AL.date::time BETWEEN NC.start::time AND NC.end::time
-        WHERE AL.state IS NULL
+        ON S."studentID" = NC."studentID" AND NC.day = ${"일월화수목금토"[new Date(new Date(start).getTime() + 9 * 60 * 60 * 1000).getDay()]}
+        WHERE AL.state IS NULL AND L.id IS NULL AND NC.id IS NULL
         `
         return NextResponse.json(
             { absenceData: absences},
